16a9c57e9f7658148c37c46fdce05a8f2c4e13ad
TinForge <29712552+TinForge@users.noreply.github.com>
2019-04-26 11:37:53 -0400

    Update TextureProcessor.cs

--- Editor/TextureProcessor.cs
+++ Editor/TextureProcessor.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEditor;
 
 /// <summary>
@@ -6,33 +6,33 @@ using UnityEditor;
 /// Platform: Android
 /// Texture size: 2048
 /// Compression: Compressed ETC2 RGBA8
-/// Compression: Best Quality
+/// Compression: Worst (0)
 /// Converts to normalmap if the word "Normal" is in its filename
 /// 
 /// Set 'OnImportOnly' to true to affect imports only, otherwise this will overwrite settings you actively apply on textures
 /// 
-/// 
 /// </summary>
 class TexturePostprocessor : AssetPostprocessor
 {
-    const bool OnImportOnly = true;
+	const bool OnImportOnly = false;
+	const int CompressionQuality = 0; // 0 for fastest compressing, 100 for best quality
 
-    void OnPreprocessTexture()
-    {
-        TextureImporter textureImporter = (TextureImporter)assetImporter;
-        Object asset = AssetDatabase.LoadAssetAtPath(textureImporter.assetPath, typeof(Texture2D));
+	void OnPreprocessTexture()
+	{
+		TextureImporter textureImporter = (TextureImporter) assetImporter;
+		Object asset = AssetDatabase.LoadAssetAtPath(textureImporter.assetPath, typeof(Texture2D));
 
-        if (asset && OnImportOnly)
-            return;
-            
+		if (asset && OnImportOnly)
+			return;
+
 		if (OnImportOnly)
 			Debug.Log("Texture Processing running");
 		else
 			Debug.LogWarning("Texture Processing overwriting settings on existing textures. This will prevent you from applying your own texture settings!");
 
-        textureImporter.SetPlatformTextureSettings("Android", 2048, TextureImporterFormat.ETC2_RGBA8, 100, false);
+		textureImporter.SetPlatformTextureSettings("Android", 2048, TextureImporterFormat.ETC2_RGBA8, CompressionQuality, false);
 
-        if (assetPath.Contains("Normal"))
-            textureImporter.normalmap = true;
-    }
+		if (assetPath.Contains("Normal"))
+			textureImporter.normalmap = true;
+	}
 }
